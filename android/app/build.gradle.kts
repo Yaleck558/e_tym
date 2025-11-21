365ff72cecebfef70b74753ff8d20b042dbe7437
Yaleck79 <thalesyyy@gmail.com>
2025-11-21 14:35:46 +0100

    first

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,6 +24,21 @@ android {
         }
     }
 }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 flutter {
     source = "../.."
 }
